810ef9b1d0cd6f893044a94668eae29c9da49a7f
Virginie <vbouvarel@lilo.org>
2021-03-18 16:07:52 +0100

    ajout gamme WIP

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 "use strict";
+
+/* Initialize swiper */
+
+var mySwiper = new Swiper('.swiper-container', {
+    effect : 'coverflow',
+    grabCursor: true,
+    centeredSlides:true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 20,
+        stretch: 0,
+        depth: 200,
+        modifier: 1,
+        slideShadows: true,
+    },
+    loop:true,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+  
+  });
